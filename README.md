e35bec8fcc8bd30aa4d74b9d2291b96c17d96300
Ian Mckay <contact@ianmckay.com.au>
2020-06-02 15:10:42 +1000

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # deepcomposer-upload
-Upload a single-track MIDI to the AWS DeepComposer service
+
+> Upload a single-track MIDI to the AWS DeepComposer service
+
+## Installation
+
+```
+npm i -g deepcomposer-upload
+```
+
+## Usage
+
+```
+deepcomposer-upload --input-filename input.mid --sample-name nameoftrack --model-id genre-rock-1 --output-filename output.mid
+```
+
+Input MIDI files should be a single track and 8 bars or less, tempo should remain static.
---
+++ main.js
@@ -0,0 +1,108 @@
+const fs = require('fs')
+const request = require('request');
+const aws4 = require('aws4');
+const AWS = require('aws-sdk');
+const parseMidi = require('midi-file').parseMidi;
+const writeMidi = require('midi-file').writeMidi;
+const cliargs = require('commander');
+const pjson = require('./package.json');
+
+function uploadSampleToDeepComposer(cliopts) {
+    const inputFh = fs.readFileSync(cliopts.inputFilename);
+
+    let chain = new AWS.CredentialProviderChain();
+    chain.resolve((err, awscreds) => {
+        let midiFile = parseMidi(inputFh);
+        let tempo = 600000;
+        let filteredactions = midiFile.tracks.pop().filter(action => {
+            if (action.type == 'setTempo') {
+                tempo = action.microsecondsPerBeat;
+            }
+            return ['noteOn', 'noteOff', 'endOfTrack'].includes(action.type);
+        });
+
+        midiFile.header = {
+            'format': 1,
+            'numTracks': 2,
+            'ticksPerBeat': midiFile.header.ticksPerBeat
+        };
+        midiFile.tracks = [
+            [
+                { deltaTime: 0, meta: true, type: 'trackName', text: '' },
+                {
+                    deltaTime: 0,
+                    meta: true,
+                    type: 'setTempo',
+                    microsecondsPerBeat: tempo
+                },
+                { deltaTime: 0, meta: true, type: 'endOfTrack' }
+            ],
+            [
+                { deltaTime: 0, meta: true, type: 'trackName', text: '' },
+                { deltaTime: 0, channel: 0, type: 'programChange', programNumber: 0 }
+            ].concat(filteredactions)
+        ];
+
+        let inputMidiB64 = Buffer.from(writeMidi(midiFile)).toString('base64');
+        let awsreq = aws4.sign({
+            service: 'deepcomposer',
+            region: 'us-east-1',
+            method: 'POST',
+            path: '/compositions',
+            headers: {
+                'Content-Type': 'application/x-amz-json-1.1',
+                'X-Amz-Target': 'DeepComposer.CreateComposition'
+            },
+            body: JSON.stringify({
+                'inputMidi': inputMidiB64,
+                'modelId': cliopts.modelId,
+                'name': cliopts.sampleName,
+                'modelType': 'SAMPLE',
+                'inputMidiSource': 'VIRTUAL'
+            })
+        }, {
+            secretAccessKey: awscreds.secretAccessKey,
+            accessKeyId: awscreds.accessKeyId
+        });
+        
+        request.post({
+            url: 'https://' + awsreq.hostname + awsreq.path,
+            headers: awsreq.headers,
+            body: awsreq.body
+        }, function(err, httpResponse, body) {
+            if (err) {
+                console.log(err);
+            }
+            try {
+                let msg = JSON.parse(body);
+                if (msg.composition) {
+                    if (cliopts.outputFilename) {
+                        fs.writeFileSync(cliopts.outputFilename, msg.composition.outputMidi, 'base64');
+                        console.log('Wrote ' + cliopts.outputFilename);
+                    } else {
+                        console.log('Uploaded MIDI file');
+                    }
+                } else {
+                    console.log(msg);
+                }
+            } catch(err) {
+                console.log(body);
+            }
+        });
+    });
+}
+
+if (require.main === module) { // if main prog.
+    let validation = false;
+    cliargs
+        .version(pjson.version)
+        .requiredOption('--input-filename <filename>', 'filename for input MIDI file')
+        .requiredOption('--sample-name <name>', 'name of the sample being uploaded')
+        .option('--model-id <id>', 'the model ID to generate against', 'genre-rock-1')
+        .option('--output-filename <filename>', 'filename for output MIDI file')
+        .action(async (opts) => {
+            await uploadSampleToDeepComposer(opts);
+        });
+
+    cliargs.parse(process.argv);
+}
